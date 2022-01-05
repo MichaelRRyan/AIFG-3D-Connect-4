89c91b9a41ea7b5d3a615e6a8595ada868e9bd45
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-05 19:51:23 +0000

    Added use of minimax

--- AIFG-3D-Connect-4/FourTechAI.cpp
+++ AIFG-3D-Connect-4/FourTechAI.cpp
@@ -9,37 +9,6 @@ FourTechAI::FourTechAI(GameBoard & t_board) :
 ///////////////////////////////////////////////////////////////////////////////
 Coordinate FourTechAI::getCoordinate()
 {
-	// Move position and score.
-	std::vector<std::pair<Coordinate, int>> moves;
-
-	// Loops through each position on the board.
-	Coordinate position;
-	for (; position.x < GameBoard::SIZE; ++position.x)
-	{
-		for (position.y = 0; position.y < GameBoard::SIZE; ++position.y)
-		{
-			for (position.z = 0; position.z < GameBoard::SIZE; ++position.z)
-			{
-				// If the position has no piece on it.
-				if (PieceType::None == m_board.getPiece(position))
-				{
-					// Gets the score of the move.
-					int moveScore = FourTechEvaluator::evaluateMove(m_board,
-						{ position, PieceType::Yellow });
-
-					// Stores the move position and score.
-					moves.push_back({ position, moveScore });
-				}
-			}
-		}
-	}
-
-	// Finds the highest scoring move.
-	auto const* bestMove = &moves.front();
-	for (auto const& move : moves)
-		if (move.second > bestMove->second)
-			bestMove = &move;
-
-	return bestMove->first; // Returns the position of the best move.
+	return Minimax::getCoordinate(m_board, PieceType::Yellow, 2);
 }
 ///////////////////////////////////////////////////////////////////////////////
